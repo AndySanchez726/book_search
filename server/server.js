09f5f2198ddfe0c0a5409864ba55f007b9629e05
Andy Sanchez <andysanchez726@gmail.com>
2020-09-02 22:34:37 -0500

    trying to fix heroku deploy

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 // app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
